bcbd88c6d12d6a8b259932b511520b89988438d1
ArseniyKorotkov <69350678+ArseniyKorotkov@users.noreply.github.com>
2022-09-05 20:33:29 +0200

    a_little_clean

--- src/by/arsy/adapters/XMLAdapter.java
+++ src/by/arsy/adapters/XMLAdapter.java
@@ -13,7 +13,6 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public final class XMLAdapter {
 
